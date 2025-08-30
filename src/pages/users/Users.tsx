67c533881f25a0f29522ce2ab677b8b093e7ccc5
codingsprints <codingsprint31@gmail.com>
2025-08-30 12:47:14 +0530

    Structuring the Filter Card

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,13 +1,37 @@
-import { RightOutlined } from "@ant-design/icons";
-import { Breadcrumb, Button, Flex, Space, Table } from "antd";
+import {
+  LoadingOutlined,
+  PlusOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
+import {
+  Breadcrumb,
+  Button,
+  Flex,
+  Form,
+  Space,
+  Spin,
+  Table,
+  Typography,
+} from "antd";
 import { Link, Navigate } from "react-router-dom";
 import { FetchUser } from "../../services/user.service";
 import { userTableColumns } from "../../components/users/UsersTable";
 import type { User } from "../../utils/types";
 import { useAuthStore } from "../../store";
+import UsersFilter from "./UserFilter";
+import { useState } from "react";
 
 const Users = () => {
-  const { data: fetchUserData } = FetchUser();
+  const [form] = Form.useForm();
+  const [filterForm] = Form.useForm();
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const {
+    data: fetchUserData,
+    isFetching: fetchDataIsFetching,
+    isError: fetchDataIsError,
+    error: fetchDataError,
+  } = FetchUser();
   console.log(fetchUserData);
 
   const { user } = useAuthStore();
@@ -27,15 +51,28 @@ const Users = () => {
               { title: "Users" },
             ]}
           />
-          {/* {isFetching && (
+          {fetchDataIsFetching && (
             <Spin
               indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}
             />
           )}
-          {isError && (
-            <Typography.Text type="danger">{error.message}</Typography.Text>
-          )} */}
+          {fetchDataIsError && (
+            <Typography.Text type="danger">
+              {fetchDataError.message}
+            </Typography.Text>
+          )}
         </Flex>
+        {/* <Form form={filterForm} onFieldsChange={onFilterChange}> */}
+        <UsersFilter>
+          <Button
+            type="primary"
+            icon={<PlusOutlined />}
+            onClick={() => setDrawerOpen(true)}
+          >
+            Add User
+          </Button>
+        </UsersFilter>
+        {/* </Form> */}
         <Table
           columns={[...userTableColumns]}
           dataSource={fetchUserData?.data?.getAllUsersDto}
